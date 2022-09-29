b2049396b33d7dcac2e5d5f749b677c0d4717c5a
Weadmead <wead758@gmail.com>
2022-09-29 04:20:39 +0300

    added header + surf section

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,38 @@
-$(function(){
+$(function () {
 
+    $('.header__slider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: true,
+        asNavFor: '.slider-dotshead',
+        prevArrow: '<img class="slider-arrows slider-arrows__left" src="images/arrow-left.svg" alt="">',
+        nextArrow: '<img class="slider-arrows slider-arrows__right" src="images/arrows-right.svg" alt="">'
+    });
 
-    
+    $('.slider-dotshead').slick({
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        asNavFor: '.header__slider',
+    });
+
+    $('.surf-slider').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1, 
+        prevArrow: '<img class="slider-arrows slider-arrows__left" src="images/arrow-left.svg" alt="">',
+        nextArrow: '<img class="slider-arrows slider-arrows__right" src="images/arrows-right.svg" alt="">',
+        asNavFor: '.slider-map',      
+    });
+
+    $('.slider-map').slick({
+        slidesToShow: 8,
+        slidesToScroll: 1,
+        arrows: false,
+        asNavFor: '.surf-slider',
+        focusOnSelect: true,
+
+    });
 });
+
+
